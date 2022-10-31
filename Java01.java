feb23e0d0558b9fa22e6503fe80d154f5f094717
littlefengsai <114210962+littlefengsai@users.noreply.github.com>
2022-10-31 21:39:54 +0800

    Add files via upload

---
+++ Java01.java
@@ -0,0 +1,22 @@
+public class Java01
+{
+	public static void main(String[] args)
+	{
+		//boolean flag = 1;
+		//boolean loginSuccess=true;
+		//if(loginSuccess){
+		//	System.out.println("¹§Ï²Äã£¬µÇÂ¼³É¹¦");
+		//}else{
+		//	System.out.println("¶Ô²»Æð£¬µÇÂ½Ê§°Ü");
+		int s = 100;
+		System.out.println(++s);
+		System.out.println(s);
+		System.out.println(--s);
+		System.out.println(s++);
+		System.out.println(s--);
+		System.out.println(s--);
+		System.out.println(s--);
+		System.out.println(s);
+	
+	}
+}
